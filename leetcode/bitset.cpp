25f297b58241868eaaf56e69e3e0831c07af1682
ArthurWangSuper <cp3wt10@163.com>
2019-04-13 00:47:13 +0800

    Update bitset.cpp

--- leetcode/bitset.cpp
+++ leetcode/bitset.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int bitadd(int a,int b)
+int add(int a,int b)
 {
     int Xor = 0;
     int Add = 0;
@@ -15,14 +15,14 @@ int bitadd(int a,int b)
 
     return a;
 }
-int bitmul(int a,int b)
+int multi(int a,int b)
 {
     int res = 0;
     while(b!=0)
     {
         if((b&1) == 1)
         {
-            res = bitadd(res,a);
+            res = add(res,a);
         }
         a<<=1;
         b>>=1;
@@ -36,35 +36,47 @@ bool isNeg(int val)
 }
 int negnum(int num)
 {
-    return bitadd(~num,1);
+    return add(~num,1);
 }
-int bitminus(int a,int b)
+int minus(int a,int b)
 {
-    return bitadd(a,negnum(b));
+    return add(a,negnum(b));
 }
 
-int bitdiv(int a,int b)
+int div(int a,int b)
 {
     int x = isNeg(a)?negnum(a):a;
     int y = isNeg(b)?negnum(b):b;
 
     int res = 0;
 
-    for(int i =31;i>-1;i=bitminus(i,1))
+    for(int i =31;i>-1;i=minus(i,1))
     {
         if((x>>i) >= y){
             res |= (1<<i);
-            x = bitminus(x,y<<i);
+            x = minus(x,y<<i);
         }
     }
     return isNeg(a)^isNeg(b)?negnum(res):res;
 }
+ int divide(int dividend, int divisor) {
+    if(dividend == INT_MIN && divisor == INT_MIN)
+        return 1;
+    else if(divisor == INT_MIN)
+        return 0;
+    else if(dividend == INT_MIN){
+        int res = (dividend+1)/divisor;      
+        return add(res,div(minus(dividend,multi(res,divisor)),divisor));
+    }
+    else 
+        return div(dividend,divisor);   
+}
 
 int main(int argc,char**argv)
 {
-    cout<<"add is:"<<bitadd(99,65)<<endl;
-    cout<<"minus is:"<<bitminus(99,65)<<endl;
-    cout<<"mul is:"<<bitmul(100,4)<<endl;
-    cout<<"div is:"<<bitdiv(100,4)<<endl;
+    cout<<"add is:"<<add(99,65)<<endl;
+    cout<<"minus is:"<<minus(99,65)<<endl;
+    cout<<"mul is:"<<multi(100,4)<<endl;
+    cout<<"div is:"<<divide(10,3)<<endl;
     return 0;
 }
