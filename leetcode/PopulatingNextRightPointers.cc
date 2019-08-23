b78702d709bac09f120669c8daf5aca5ea805013
ArthurWangSuper <cp3wt10@163.com>
2019-08-23 20:19:21 +0800

    Create PopulatingNextRightPointers.cc

---
+++ leetcode/PopulatingNextRightPointers.cc
@@ -0,0 +1,40 @@
+// leetcode 119
+// You are given a [perfect binary tree] where all leaves are on the same level,and every parent has two children.
+// The binary tree has the following definition:
+// Populate each next pointer to point to its next right node. 
+// If there is no next right node, the next pointer should be set to NULL.
+// Initially, all next pointers are set to NULL.
+
+#include <iostream>
+
+class Node{
+	int val;
+	Node *left;
+	Node *right;
+	Node *next;
+	Node() {}
+	Node(int _val,Node* _left,Node* _right,Node* _next){
+		val = _val;
+		left = _left;
+		right = _right;
+		next = _next;
+	}
+};
+
+class Solution{
+public:
+	Node* connect(Node* root){
+		if(!root || !root->left) return root;
+		
+		root->left->next = root->right;
+		
+		if(root->next)
+			root->right->next = root->next->left;
+		
+		connect(root->left);
+		connect(root->right);
+		
+		return root;
+		
+	}
+};
