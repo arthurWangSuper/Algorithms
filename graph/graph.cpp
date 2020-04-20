88d3eb35eadea0183d8dd74aca2e23f9742ce591
wangtao <wangtao@wangtaodeMacBook-Pro.local>
2020-04-20 09:21:45 +0800

    add some leetcode

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,20 +1,30 @@
 #include<iostream>
 #include<vector>
+class graph_matrix{
+private:
+	vertexType vex[MAXVEX];
+	int MAT[MAXVEX][MAXVEX];
+	int vexNum,EdgeNum;
+};
+
 
 class graph
 {
 	private:
+		// 边表节点
 		struct EdgeNode
 		{
 			int adjvex;
 			int weight;
 			EdgeNode *next;
 		};
+		// 顶点节点
 		struct VertexNode
 		{	
 			int data;
 			EdgeNode *firstedge;
 		};
+	
 		struct GraphAdjList
 		{
 			vertexNode *Vertexlist;
